c5b7d3c26393e44e705e3c0f11448c05021b85c0
Sanjib Dhar <R041411012@recursion.com.bd>
2018-10-31 19:38:30 +0600

    add routing part

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Blog from "./components/Blog";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Example from "./components/Example";
 
 export default class Index extends Component {
     render() {
         return (
             <div className="container">
+                <Router>
+                    <div>
+                        <Link to="/">Home</Link>
+                        <Link to="/blog">blog</Link>
+                        <Route path="/" Component={Example} />
+                        <Route path="/" Component={Blog} />
+                    </div>
+                </Router>
+
                 <Blog />
             </div>
         );
